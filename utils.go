84286e81b38ac89e6ce390c6eed31f51014b8fe9
Patrick Kabwe <patrickckabwe@gmail.com>
2024-03-14 08:17:16 +0200

    chore: create auth func

--- utils.go
+++ utils.go
@@ -1,9 +1,13 @@
 package notify_sms
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -34,4 +38,34 @@ func makeRequest(method, endpoint string, params io.Reader, opt MakeRequestOptio
 	}
 
 	return resBody, nil
+}
+
+func auth(username, password, baseURL string) error {
+	endpoint := fmt.Sprintf("%s/authentication/web/login?error_context=CONTEXT_API_ERROR_JSON", baseURL)
+	bodyInBytes := []byte(fmt.Sprintf(`{"username": "%s", "password": "%s"}`, username, password))
+
+	bodyReader := bytes.NewReader(bodyInBytes)
+	res, err := makeRequest(http.MethodPost, endpoint, bodyReader, MakeRequestOptions{})
+	var authResponse APIResponse[AuthAPIResponse]
+
+	if err != nil {
+		log.Printf(ErrorPrefix+"/%s\n", err)
+		os.Exit(1)
+	}
+
+	err = json.Unmarshal(res, &authResponse)
+
+	if err != nil {
+		log.Printf(ErrorPrefix+"/%s\n", err)
+		os.Exit(1)
+	}
+
+	if !authResponse.Success {
+		log.Printf(ErrorPrefix+"/%s\n", err)
+		return InvalidCredErr
+	}
+
+	tokenCache["token"] = authResponse.Payload.Token
+
+	return nil
 }
