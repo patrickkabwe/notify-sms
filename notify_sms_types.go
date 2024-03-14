a232e3365a6c2c45ccc953d7d19010102cd3d25f
Patrick Kabwe <patrickckabwe@gmail.com>
2024-03-14 08:13:44 +0200

    chore: generate types

---
+++ notify_sms_types.go
@@ -0,0 +1,111 @@
+package notify_sms
+
+import (
+	"time"
+)
+
+type RecipientType string
+type SenderID string
+type Token string
+
+type MakeRequestOptions struct {
+	Headers map[string]string
+}
+
+type APIResponse[T any] struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+	Payload T      `json:"payload"`
+}
+
+type AuthAPIResponse struct {
+	Token Token `json:"token"`
+}
+
+type SenderResponse struct {
+}
+
+type SendersAPIResponse struct {
+	Data []Sender `json:"data"`
+}
+type SenderAPIResponse struct {
+	Data Sender `json:"data"`
+}
+
+type Sender struct {
+	Id             string    `json:"_id"`
+	Title          string    `json:"title"`
+	Description    string    `json:"description"`
+	Tracker        Tracker   `json:"tracker"`
+	Status         string    `json:"status"`
+	Active         bool      `json:"active"`
+	Account        string    `json:"account"`
+	User           string    `json:"user"`
+	CreatedOn      time.Time `json:"createdOn"`
+	LastModifiedOn time.Time `json:"lastModifiedOn"`
+}
+
+type Tracker struct {
+	Id             string    `json:"_id"`
+	Title          string    `json:"title"`
+	AutoApprove    bool      `json:"autoApprove"`
+	Status         string    `json:"status"`
+	Active         bool      `json:"active"`
+	CreatedOn      time.Time `json:"createdOn"`
+	LastModifiedOn time.Time `json:"lastModifiedOn"`
+}
+
+type CreateSenderIDParams struct {
+	BusinessName string
+	Description  string
+}
+
+type NewClientParams struct {
+	UserName string `json:"userName"`
+	Password string `json:"password"`
+}
+
+type SendSmsToChannelParams struct {
+	RecipientType RecipientType `json:"recipientType"`
+	// SenderID - sender id collected from GetSenderIDs
+	SenderID string `json:"senderId"`
+	// Channel - channel you are trying to send sms message to.
+	Channel string
+	// Message - Text message you are sending to contact(s)
+	// e.g Hello Notify
+	Message string `json:"message"`
+}
+
+type SendSmsToCustomContactsParams struct {
+	RecipientType RecipientType `json:"reciepientType"`
+	// SenderID - sender id collected from GetSenderIDs
+	SenderID string `json:"senderId"`
+	// Contacts - phone numbers you are trying to send sms to.
+	// e.g [+260979600000]
+	Contacts []string `json:"reciepients"`
+	// Message - Text message you are sending to contact(s)
+	// e.g Hello Notify
+	Message string `json:"message"`
+}
+
+type SendSmsToContactGroup struct {
+	RecipientType RecipientType `json:"recipientType"`
+	// SenderID - sender id collected from GetSenderIDs
+	SenderID string `json:"senderId"`
+	// ContactGroup - The group id you are trying to send a sms message.
+	// PS: Only define this field when you are trying to send sms to a specific group.
+	ContactGroup string
+	// Message - Text message you are sending to contact(s)
+	// e.g Hello Notify
+	Message string
+}
+
+type SendSmsParams struct {
+	// RecipientType - can be defined as either
+	//
+	// 1. **NOTIFY_RECIPIENT_TYPE_CHANNEL** - used when sending a sms to a channel.
+	//
+	// 2. **NOTIFY_RECIPIENT_TYPE_CUSTOM** - used when sending a sms to a list of contacts.
+	//
+	// 3. **NOTIFY_RECIPIENT_TYPE_CONTACT_GROUP** - used when sending a sms to a contact group.
+}
